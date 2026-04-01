62a63658d167170f1931d8f05541c7586d392018
= <alifdaniel279@gmail.com>
2026-04-02 00:57:58 +0800

    updated message.sql

--- message.sql
+++ message.sql
@@ -1,4 +1,9 @@
-CREATE TABLE messages (
+CREATE TABLE IF NOT EXISTS messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
     message TEXT
 );
+
+-- ADD THESE LINES:
+INSERT INTO messages (message) VALUES ('Hello from the database!');
+INSERT INTO messages (message) VALUES ('DevOps is awesome!');
+INSERT INTO messages (message) VALUES ('Docker Compose is working!');
